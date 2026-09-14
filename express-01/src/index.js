fba8967eed60a997f042577b2fab73adc769ef17
Arthur <arthurms707@gmail.com>
2026-09-14 16:54:26 -0300

    Adicionando as atualizações da aula

--- express-01/src/index.js
+++ express-01/src/index.js
@@ -1,94 +1,43 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import models, { sequelize } from "./models/index.js";
+import routes from "./routes/index.js";
 
 const app = express();
 
-app.set('trust proxy', true); 
+app.set("trust proxy", true);
+
+// middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// middleware the logs
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
-
-app.get('/', (req, res) => {
-  return res.send('Servidor express exectuando...');
-});
-
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
-
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
-
-app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
-});
-
-app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
-});
-
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
-});
-
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
-
-app.post('/messages', (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body?.text,
+// middleware de autenticação "fake" + injeção dos models no req.context
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
   };
-
-  messages[id] = message;
-
-  return res.send(message);
+  next();
 });
 
+// rotas
+app.get("/", (req, res) => {
+  return res.send("Servidor express exectuando...");
+});
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`),
-);
+const eraseDatabaseOnSync = process.env.ERASE_DATABASE_ON_SYNC === "true";
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
---
+++ express-01/src/models/index.js
@@ -0,0 +1,25 @@
+import Sequelize from "sequelize";
+import pg from "pg";
+
+import getUserModel from "./user.js";
+import getMessageModel from "./message.js";
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectModule: pg,
+});
+
+const models = {
+  User: getUserModel(sequelize, Sequelize),
+  Message: getMessageModel(sequelize, Sequelize),
+};
+
+Object.keys(models).forEach((key) => {
+  if ("associate" in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+export { sequelize };
+
+export default models;
